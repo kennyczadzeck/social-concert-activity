782f8823c7bb0758adca1d4db91094643c5f1dd8
Kenny Czadzeck <kennycgit@gmail.com>
2015-02-07 15:32:55 -0800

    client is now displaying info boxes at the coordinates in the server's response. using temporary titles and content.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,4 @@
 
-
-// $.get('/googleMaps', function(response){
-//   console.log(response)
-//   eval(response.body)
-// });
-
 function initialize() {
   navigator.geolocation.getCurrentPosition(function(data) {
     var mapOptions = {
@@ -23,7 +17,9 @@ function eqfeed_callback(results) {
   map.data.addGeoJson(results);
 }  
 
+// DOCUMENT.READY
 $(function() {
+
   $('#show-search').on('submit', function(event){
     event.preventDefault();
     var formData = {
@@ -33,11 +29,7 @@ $(function() {
     };
     formData.city = formData.city.toLowerCase();
     formData.region = formData.region.toLowerCase();
-    $.ajax({
-      url: '/bandsInTown',
-      data: formData,
-      success: function(res){console.log(res)}
-    });
+
     $.get('https://maps.googleapis.com/maps/api/geocode/json?address='+formData.city+',+'+formData.region+'&key=AIzaSyDADcp3AczDleifGLCOrAusMgPq_GUyXR4', function(response){
       var coordinates = response.results[0].geometry.location;
       var mapOptions = {
@@ -45,7 +37,32 @@ $(function() {
           center: {lat: coordinates.lat, lng: coordinates.lng},
           mapTypeId: 'roadmap'
         };
-      return new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+      var newMap = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+      $.ajax({
+        url: '/bandsInTown',
+        data: formData,
+        success: function(res){
+          console.log(res);
+          res.forEach(function(show) {
+            var imageURL = show.instagramMedia[0].images.low_resolution.url
+            var contentWindow = new google.maps.InfoWindow({
+              content: "<img src="+imageURL+">"
+            })
+            var marker = new google.maps.Marker({
+              position: {lat: show.coordinates.latitude, lng: show.coordinates.longitude},
+              map: newMap,
+              title: show.artists[0]
+            });
+            google.maps.event.addListener(marker, 'click', function() {
+              contentWindow.open(newMap,marker);
+            });
+          })
+        }
+      });
+
+      return newMap
     });
   });
+
 });
